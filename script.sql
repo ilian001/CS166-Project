60b47ab9d0795461b30deddb3afce0f47a1a6f3e
Ivan Liang <ilian001@wch133-10.cs.ucr.edu>
2020-08-06 12:37:23 -0700

    debug

--- script.sql
+++ script.sql
@@ -1,20 +1,20 @@
 CREATE TABLE customer(	CID		INTEGER NOT NULL,
 			first_name	CHAR(20),
 			last_name	CHAR(20),
-			phone_number	INTEGER
+			phone_number	INTEGER,
 			address 	CHAR(50),
-			PRIMARY KEY (CID),
-			FOREIGN KEY (VIN) REFERENCES cars )
+			PRIMARY KEY (CID) );
+			--FOREIGN KEY (VIN) REFERENCES cars );
 				
 
 CREATE TABLE cars(	VIN		INTEGER NOT NULL,
 			make		CHAR(20),
 			model		CHAR(20),
 			year		INTEGER,
-			PRIMARY KEY(VIN),
-			FOREIGN KEY(CID) REFERENCES customer,
-			FOREIGN KEY(employee_id) REFERENCES mechanic,
-			FOREIGN KEY(SID) REFERENCES serviceRequest )
+			PRIMARY KEY(VIN) );
+			--FOREIGN KEY(CID) REFERENCES customer,
+			--FOREIGN KEY(employee_id) REFERENCES mechanic,
+			--FOREIGN KEY(SID) REFERENCES serviceRequest );
 				
 
 
@@ -22,9 +22,9 @@ CREATE TABLE mechanic(	first_name 	CHAR(20),
 			last_name	CHAR(20),
 			employee_id	INTEGER NOT NULL,	
 			years_of_exp	INTEGER, 
-			PRIMARY KEY(employee id)
-			FOREIGN KEY(VIN),
-			FOREIGN KEY(SID) )
+			PRIMARY KEY(employee_id) );
+			--FOREIGN KEY(VIN) REFERENCES cars,
+			--FOREIGN KEY(SID) REFERENCES customer);
 				
 CREATE TABLE serviceRequest(	SID			INTEGER NOT NULL,
 				problem 		CHAR(20),
@@ -34,33 +34,33 @@ CREATE TABLE serviceRequest(	SID			INTEGER NOT NULL,
 				status			CHAR(20),
 				close_date		DATE,
 				open_date		DATE,
-				PRIMARY KEY(SID), 
-				FOREIGN KEY(employee id) REFERENCES Mechanic, 
-				FOREIGN KEY(VIN) REFERENCES Cars ) 
+				PRIMARY KEY(SID) );
+				--FOREIGN KEY(employee_id) REFERENCES Mechanic, 
+				--FOREIGN KEY(VIN) REFERENCES Cars );
 
 CREATE TABLE checks (	VIN 		INTEGER NOT NULL,
 			SID		INTEGER, 
 			drop_off 	CHAR(20),
-			PRIMARY KEY(VIN,SID), 
-			FOREIGN KEY(VIN) REFERENCES Cars,
-			FOREIGN KEY(SID) REFERENCES serviceRequest )
+			PRIMARY KEY(VIN,SID) );
+			--FOREIGN KEY(VIN) REFERENCES Cars,
+			--FOREIGN KEY(SID) REFERENCES serviceRequest );
 
 
 CREATE TABLE owns (	CID		INTEGER NOT NULL,
 			VIN		INTEGER NOT NULL,
-			PRIMARY KEY(CID,VIN),
-			FOREIGN KEY(CID) REFERENCES Customer,
-			FOREIGN KEY(VIN) REFERENCES Cars) 
+			PRIMARY KEY(CID,VIN) );
+			--FOREIGN KEY(CID) REFERENCES Customer,
+			--FOREIGN KEY(VIN) REFERENCES Cars); 
 
-CREATE TABLE workson (	employeeID 	INTEGER NOT NULL,
+CREATE TABLE workson (	employee_id 	INTEGER NOT NULL,
 			VIN		INTEGER NOT NULL,
-			PRIMARY KEY(employee id, VIN), 
-			FOREIGN KEY(employee ID) REFERENCES mechanic,
-			FOREIGN KEY(VIN) REFERENCES cars )
+			PRIMARY KEY(employee_id, VIN) ); 
+			--FOREIGN KEY(employee_id) REFERENCES mechanic,
+			--FOREIGN KEY(VIN) REFERENCES cars );
 
-CREATE TABLE creates (	employeeID	INTEGER NOT NULL,
+CREATE TABLE creates (	employee_id	INTEGER NOT NULL,
 			SID		INTEGER NOT NULL,
-			PRIMARY KEY(employeeID, SID),
-			FOREIGN KEY(employeeID) REFERENCES mechanic,
-			FOREIGN KEY(SID) REFERENCES serviceRequest ) 					
+			PRIMARY KEY(employee_id, SID) );
+			--FOREIGN KEY(employee_id) REFERENCES mechanic,
+			--FOREIGN KEY(SID) REFERENCES serviceRequest ); 					
 
